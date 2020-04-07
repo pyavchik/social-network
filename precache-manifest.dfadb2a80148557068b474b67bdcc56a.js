8353a34230f1edbd88bd8d89a250f33239281f2c
pyavchik <pyavchik@gmail.com>
2020-04-07 19:05:41 +0300

    Updates

---
+++ precache-manifest.dfadb2a80148557068b474b67bdcc56a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca7287e2ea94d5c7d4742553f9d3b607",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "59661d583727ff815126",
+    "url": "/social-network/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58db72ec723f70db0986",
+    "url": "/social-network/static/js/2.692a7f06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-network/static/js/2.692a7f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59661d583727ff815126",
+    "url": "/social-network/static/js/main.aa2d5f77.chunk.js"
+  },
+  {
+    "revision": "f23d3fb05ab6b682feb1",
+    "url": "/social-network/static/js/runtime-main.f6cfeeaa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/social-network/static/media/logo.5d5d9eef.svg"
+  }
+]);
